12fdc4040468379e8a3a1c69aa72418504dbdbe2
ItzAnsh <anshbhasin3723@gmail.com>
2024-08-30 02:19:22 +0530

    update: package json updated + booking-service changes

--- apps/booking-service/src/booking-service.module.ts
+++ apps/booking-service/src/booking-service.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BookingServiceController } from './booking-service.controller';
 import { BookingServiceService } from './booking-service.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule],
   controllers: [BookingServiceController],
   providers: [BookingServiceService],
 })
